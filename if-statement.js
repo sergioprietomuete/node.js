fd4d82d89b49215b42f2a34e32fde5989aa55b1a
Sergio Prieto Muete <3914754+sergioprietomuete@users.noreply.github.com>
2018-05-14 23:34:40 -0500

    Delete if-statement.js

--- if-statement.js
+++
@@ -1,7 +0,0 @@
-var fruit = 'orange';
-fruit = fruit.length
-if (fruit > 1) {
-	console.log(`The fruit name has more than five characters.`);
-} else {
-	console.log(`The fruit name has five characters or less.`);
-}
